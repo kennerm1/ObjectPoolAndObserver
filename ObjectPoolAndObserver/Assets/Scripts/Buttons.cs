a9aa30de9cb51a7089aef87a4aac5a43c5ded36d
kennerm1 <123021312+kennerm1@users.noreply.github.com>
2024-09-26 11:28:36 -0700

    build

---
+++ ObjectPoolAndObserver/Assets/Scripts/Buttons.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour
+{
+    public void toObserver()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void toObjPool()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
